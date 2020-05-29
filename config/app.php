dd7f9990f054ba4a7956682a854f89dedbb93ebb
Alejo <alejosegi@gmail.com>
2020-05-29 12:14:45 +0200

    Setting datatables plugins

--- config/app.php
+++ config/app.php
@@ -165,6 +165,9 @@ return [
         /*
          * Package Service Providers...
          */
+        Yajra\DataTables\DataTablesServiceProvider::class,
+        Yajra\DataTables\HtmlServiceProvider::class,
+        Yajra\DataTables\ButtonsServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -229,6 +232,7 @@ return [
         'Form'      => Collective\Html\FormFacade::class,
         'Html'      => Collective\Html\HtmlFacade::class,
         'Flash'     => Laracasts\Flash\Flash::class,
+        'DataTables' => Yajra\DataTables\Facades\DataTables::class,
     ],
 
 ];
