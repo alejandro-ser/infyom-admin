f943604fbfaa27ffde63e170d1acd08f5bcf944c
Alejo <alejosegi@gmail.com>
2020-06-06 13:17:38 +0200

    Seeds for user model

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,13 +4,17 @@
 
 use App\User;
 use Faker\Generator as Faker;
+use Illuminate\Support\Facades\Hash;
 
 $factory->define(User::class, function (Faker $faker) {
 
+    $name = $faker->word;
+    $lastName = $faker->word;
+
     return [
-        'name' => $faker->word,
-        'email' => $faker->word,
-        'password' => $faker->word,
+        'name' => $name.' '.$lastName,
+        'email' => $name.'-'.$lastName.'@correo.dev',
+        'password' => Hash::make('12345678'),
         'created_at' => $faker->date('Y-m-d H:i:s'),
         'updated_at' => $faker->date('Y-m-d H:i:s')
     ];
